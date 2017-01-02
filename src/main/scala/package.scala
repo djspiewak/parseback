b8606ee182376f44912f19d6e26a91971fe8b5fc
Daniel Spiewak <djspiewak@gmail.com>
2017-01-01 22:12:42 -0700

    Removed a couple more things from the public API

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -4,10 +4,10 @@ package object parseback {
   type ~[+A, +B] = (A, B)
   val ~ = Tuple2
 
-  type \/[+A, +B] = Either[A, B]
+  private[parseback] type \/[+A, +B] = Either[A, B]
 
-  def -\/[A, B](a: A): Either[A, B] = Left(a)
-  def \/-[A, B](b: B): Either[A, B] = Right(b)
+  private[parseback] def -\/[A, B](a: A): Either[A, B] = Left(a)
+  private[parseback] def \/-[A, B](b: B): Either[A, B] = Right(b)
 
   final implicit class LazyParserSyntax[A](self: => Parser[A]) {
     def |(that: => Parser[A]): Parser[A] = Parser.Union(() => self, () => that)
