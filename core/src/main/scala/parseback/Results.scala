ae7701a6cf5f204fe25046091286db16ff97f46c
Daniel Spiewak <djspiewak@gmail.com>
2017-02-04 07:58:43 -0700

    Fixed error prioritization through sequence to match prior semantics

--- core/src/main/scala/parseback/Results.scala
+++ core/src/main/scala/parseback/Results.scala
@@ -14,13 +14,14 @@ sealed trait Results[+A] {
     case Hypothetical(errors) => Hypothetical(errors)
   }
 
+  // error merging is not commutative
   final def &&[B](that: Results[B]): Results[(A, B)] = (this, that) match {
     case (Success(v1), Success(v2)) => Success(v1 flatMap { a => v2 map { b => (a, b) } })
     case (Success(_), Failure(e)) => Failure(e)
     case (Failure(e), Success(_)) => Failure(e)
-    case (Failure(e1), Failure(e2)) => Failure(ParseError.prioritize(e1 ::: e2))
-    case (Failure(e), Hypothetical(h)) => Failure(ParseError.prioritize(e ::: h))
-    case (Hypothetical(h), Failure(e)) => Failure(ParseError.prioritize(e ::: h))
+    case (Failure(e), Failure(_)) => Failure(e)
+    case (Failure(e), Hypothetical(_)) => Failure(e)
+    case (Hypothetical(h), Failure(_)) => Failure(h)
     case (Success(_), Hypothetical(h)) => Hypothetical(h)
     case (Hypothetical(h), Success(_)) => Hypothetical(h)
     case (Hypothetical(h1), Hypothetical(h2)) => Hypothetical(ParseError.prioritize(h1 ::: h2))
